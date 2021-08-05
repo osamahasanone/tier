f7a7dd8004a3cb90c58358ec1e361c89ef309008
Osama <osamahasanone@gmail.com>
2021-08-05 20:15:53 +0300

    fix bug saving normal text in url

--- urlshortener/models/url.py
+++ urlshortener/models/url.py
@@ -6,21 +6,9 @@ from ..constants import HASH_NBYTES
 from ..constants import BASE_URL
 
 
-class URLQuerySet(models.QuerySet):
-    def shorten(self, long_url):
-        url = self.filter(long_text=long_url).first()
-        if not url:
-            url = URL(long_text=long_url)
-            url.save()
-        url.visit()
-        return url
-
-
 class URL(models.Model):
     long_text = models.URLField(max_length=1000, unique=True)
     hash = models.CharField(max_length=8, unique=True)
-
-    objects = URLQuerySet.as_manager()
 
     def __str__(self):
         return self.short_text
--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import URL
 from .serializers import URLSerializerResponse, URLSerializerShortenResponse, URLSerializerRequest
 
@@ -14,12 +15,25 @@ class URLDetail(generics.RetrieveAPIView):
     serializer_class = URLSerializerResponse
 
 
-class URLShortener(generics.CreateAPIView):
-    serializer_class = URLSerializerRequest
+class URLShortener(APIView):
+
+    def get(self, request, format=None):
+        long_url = request.data.get('long_text')
+        if not long_url:
+            return Response('Bad Request', status=status.HTTP_400_BAD_REQUEST)
+        url = URL.objects.filter(long_text=long_url).first()
+        if not url:
+            return Response('Not Found', status=status.HTTP_404_NOT_FOUND)
+        url.visit()
+        return Response(URLSerializerShortenResponse(url).data)
 
     def post(self, request, format=None):
         long_url = request.data.get('long_text')
         if not long_url:
             return Response('Bad Request', status=status.HTTP_400_BAD_REQUEST)
-        url = URL.objects.shorten(long_url)
-        return Response(URLSerializerShortenResponse(url).data)
+        serializer = URLSerializerRequest(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        saved_url = serializer.save()
+        saved_url.visit()
+        return Response(URLSerializerShortenResponse(saved_url).data)
